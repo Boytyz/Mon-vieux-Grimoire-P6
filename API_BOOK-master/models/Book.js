82c945f9faefc8cf1a169ffcc074f63b2cbc9d19
Léobouat <leobouat6@gmail.com>
2025-09-08 13:27:48 +0200

    models

--- API_BOOK-master/models/Book.js
+++ API_BOOK-master/models/Book.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+// Schema du livre
 const bookSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   title: { type: String, required: true },
--- API_BOOK-master/models/User.js
+++ API_BOOK-master/models/User.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+// Schema du user
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true, minlength: 5 },
